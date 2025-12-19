92726686fee93a0883b4fbd0ac97b42689fd25ca
csoegi <c_koko_inter@hotmail.com>
2025-12-20 02:17:10 +0700

    Configure vercel blob and allow remote pattern

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images: {
       remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-          port: '',
-         pathname: '/photo-1483478550801-ceba5fe50e8e',
-       },
-       {
-         protocol: 'https',
-         hostname: 'images.unsplash.com',
-         port: '',
-         pathname: '/photo-1526378722484-bd91ca387e72',
-       }
-     ],
+      {
+        protocol: 'https',
+        hostname: '7qbb6tluxkzf6t8y.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true, // ignore lint build error
