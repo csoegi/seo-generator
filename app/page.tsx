1ae5a88649f3cf601f3ac51babe332cf8ccf34cf
csoegi <c_koko_inter@hotmail.com>
2025-12-20 02:25:28 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
+import { Settings2 } from "lucide-react";
 import { Fragment } from "react";
 import OfficialDebuggers from "./_components/debugger";
+import GenerateSitemap from "./_components/generate-sitemap";
 import GenerateTags from "./_components/generate-tags";
 import Header from "./_components/header";
 import { previews } from "./_components/preview/previews";
@@ -11,7 +14,7 @@ import { Settings } from "./_components/settings";
 
 export default function Home() {
   return (
-    <main aria-labelledby="container" className={cn("mx-auto max-w-6xl px-8 pb-12 xl:px-4")}>
+    <main aria-labelledby="container" className={cn("mx-auto max-w-5xl px-8 pb-12 xl:px-4")}>
       <Header />
       <section className="mt-10 items-start justify-center gap-20 md:grid md:grid-cols-2">
         <div className={cn("h-fit overflow-y-auto md:sticky md:-top-6 lg:pr-10 xl:pr-20")}>
@@ -19,18 +22,27 @@ export default function Home() {
           <SeoForm />
           <OfficialDebuggers />
           <div className="mt-10">
-            <Label className="mb-6">Extras</Label>
+            <Label className="mb-6">Settings</Label>
             <Settings />
-            <a href="template.html" target="_blank">
-              <span className="pointer-events-none flex-1">View Template</span>
-            </a>
             {/* <Rating /> */}
+          </div>
+          <div className="mt-10">
+            <Label className="mb-6">Template</Label>
+            <Button onClick={() => 
+              window.open('template.html')} 
+              className="cursor-pointer bg-blue-600 text-white after:flex-1 hover:bg-blue-500/90">
+              <span className="pointer-events-none flex-1">
+                <Settings2 className="opacity-60" size={16} aria-hidden="true" />
+              </span>
+              View Template
+            </Button>
           </div>
         </div>
         <div>
           <div className="flex items-center justify-between">
             <p className={"containerTitle"}>Preview</p>
             <GenerateTags />
+            <GenerateSitemap />
           </div>
           <div className="mt-6 space-y-10">
             {previews.map(({ name, component: Component }) => (
